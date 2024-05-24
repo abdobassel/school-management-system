d09715e7bc0bc03df246131bb61532d119f26d36
abdobassel <abdobassel@gmail.com>
2024-05-24 18:54:21 +0300

    refactor trans en nameclass

--- resources/lang/en/My_Classes_trans.php
+++ resources/lang/en/My_Classes_trans.php
@@ -13,7 +13,7 @@ return [
     'submit' => 'submit',
     'required_ar' => 'Please Enter The class Name in Arabic',
     'required_en' => 'Please Enter The class Name in English',
-    'Name_class_en' => 'Name class en',
+    'Name_class' => 'Name class en',
     'Name_Grade' => 'Name Grade',
     'add_row' => 'add row',
     'delete_row' => 'Delete row',
