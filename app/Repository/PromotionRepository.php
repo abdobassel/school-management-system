35ee629cce466460075dc348e22f0043b5554a3c
abdobassel <abdobassel@gmail.com>
2024-06-05 20:56:15 +0300

    Promotion Repository => success insert to promotions table no errors or dublicate

--- app/Repository/PromotionRepository.php
+++ app/Repository/PromotionRepository.php
@@ -4,6 +4,8 @@ namespace App\Repository;
 
 use App\Grade;
 use App\Student;
+use App\Promotion;
+use Illuminate\Support\Facades\DB;
 use App\Repository\PromotionRepositoryInterface;
 
 class PromotionRepository implements PromotionRepositoryInterface
@@ -15,7 +17,7 @@ class PromotionRepository implements PromotionRepositoryInterface
     }
     public function store($request)
     {
-
+        DB::beginTransaction(); // two tables
         // steps /////////////////////////////////////////////////////////////////////////////////////////////////////////////
         //1 update student table 
         // 2 => student_id => get all student related $reqest->grade-section-classroom => get students
@@ -25,25 +27,40 @@ class PromotionRepository implements PromotionRepositoryInterface
         //3 => insert to promotions table if not exists - if exists => update
         // more than stuednt => needs foreach => array => inarray(); => ids
         //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        $students = Student::where('grade_id', $request->Grade_id)->where('classroom_id', $request->Classroom_id)->where('section_id', $request->section_id)->get();
-        foreach ($students as $student) {
-            $ids = explode(',', $student->id);
-            $student::whereIn('id', $ids)->update([
-                'grade_id' => $request->Grade_id_new,
-                'classroom_id' => $request->Classroom_id_new,
-                'section_id' => $request->section_id_new
-            ]);
-        }
+        try {
+            $students = Student::where('grade_id', $request->Grade_id)->where('classroom_id', $request->Classroom_id)->where('section_id', $request->section_id)->get();
 
-        /*
-          "Grade_id": "1",
-  "Classroom_id": "5",
-  "section_id": "6",
-  "academic_year": "2025",
-  "Grade_id_new": "2",
-  "Classroom_id_new": "4",
-  "section_id_new": "5",
-  "academic_year_new": "2024"
-        */
+            if ($students->count() < 1) {
+                return redirect()->back()->withErrors(['error' => 'Not Found Studnts']);
+            }
+            foreach ($students as $student) {
+                $ids = explode(',', $student->id);
+                $student::whereIn('id', $ids)->update([
+                    'grade_id' => $request->Grade_id_new,
+                    'classroom_id' => $request->Classroom_id_new,
+                    'section_id' => $request->section_id_new
+                ]);
+
+                Promotion::updateOrCreate([
+
+                    'student_id' => $student->id,
+                    'from_Classroom' => $request->Classroom_id,
+                    'from_grade' => $request->Grade_id,
+                    'from_section' => $request->section_id,
+
+                ], [
+                    'to_grade' => $request->Grade_id_new,
+                    'to_Classroom' => $request->Classroom_id_new,
+                    'to_section' => $request->section_id_new,
+
+                ]);
+            }
+            DB::commit(); // two tables without errors 
+            toastr()->success(trans('messages.success'));
+            return redirect()->back();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return redirect()->back()->withErrors(['error' => $e->getMessage()]);
+        }
     }
 }
